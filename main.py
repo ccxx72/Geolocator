43b1d6b5193178f41ed1dbb354d65ce8013e88cd
progressify <a.porcelli90@gmail.com>
2020-10-21 13:17:05 +0200

    pep8 optimization now i will select always the first sheet i have removed the x variable in favour of the mod 10 check

--- main.py
+++ main.py
@@ -1,40 +1,44 @@
 from geopy.geocoders import Photon
 from openpyxl import load_workbook
 
+LAT_COLUMN = 23
+LON_COLUMN = 24
+PRECISION_COLUMN = 25
+
 
 def main():
     geolocator = Photon()
 
     wb = load_workbook(filename='File.xlsx')
-    sheet = wb['Foglio1']
+    sheets = wb.sheetnames
+    sheet = wb[sheets[0]]  # selecting always the first sheet
     row_count = sheet.max_row
     column_count = sheet.max_column
-    x = 0
     for i in range(2, row_count):
-        # if sheet.cell(row=i, column=25).value == '0':
-        x = x + 1
-        if x == 10:
+        if i % 10 == 0:
             wb.save("File.xlsx")
-            x = 0
-        indirizzo = str(sheet.cell(row=i, column=5).value) + ' ' + str(sheet.cell(row=i, column=7).value) + ' ' + str(sheet.cell(row=i, column=6).value) + ' ITALIA'
-        print(x, indirizzo)
+        indirizzo = str(sheet.cell(row=i, column=5).value) + ' ' + \
+                    str(sheet.cell(row=i, column=7).value) + ' ' + str(sheet.cell(row=i, column=6).value) + ' ITALIA'
+        print(i, indirizzo)
         print(str(sheet.cell(row=i, column=23).value), str(sheet.cell(row=i, column=24).value))
         try:
             location = geolocator.geocode(indirizzo, timeout=10)
-            sheet.cell(row=i, column=23).value = location.latitude
-            sheet.cell(row=i, column=24).value = location.longitude
-            sheet.cell(row=i, column=25).value = ''
+            sheet.cell(row=i, column=LAT_COLUMN).value = location.latitude
+            sheet.cell(row=i, column=LON_COLUMN).value = location.longitude
+            sheet.cell(row=i, column=PRECISION_COLUMN).value = ''
             print((location.latitude, location.longitude))
         except:
-            location = geolocator.geocode(str(sheet.cell(row=i, column=6).value) + ' ' + str(sheet.cell(row=i, column=7).value) + ' ITALIA', timeout=10)
-            sheet.cell(row=i, column=23).value = location.latitude
-            sheet.cell(row=i, column=24).value = location.longitude
-            sheet.cell(row=i, column=25).value = '0'
+            location = geolocator.geocode(str(sheet.cell(row=i, column=6).value) + ' ' +
+                                          str(sheet.cell(row=i, column=7).value) + ' ITALIA', timeout=10)
+            sheet.cell(row=i, column=LAT_COLUMN).value = location.latitude
+            sheet.cell(row=i, column=LON_COLUMN).value = location.longitude
+            sheet.cell(row=i, column=PRECISION_COLUMN).value = '0'
             print('NP')
         else:
-            sheet.cell(row=i, column=23).value = ''
-            sheet.cell(row=i, column=24).value = ''
-            sheet.cell(row=i, column=25).value = '-1'
+            sheet.cell(row=i, column=LAT_COLUMN).value = ''
+            sheet.cell(row=i, column=LON_COLUMN).value = ''
+            sheet.cell(row=i, column=PRECISION_COLUMN).value = '-1'
+
     wb.save("File.xlsx")
 
 
